33df9ecd1abec12986126dd7e263e93fb8b8c594
LBL147 <3125748117@qq.com>
2025-07-18 17:29:08 +0800

    商户查询缓存Redis

--- hm-dianping/hm-dianping/src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ hm-dianping/hm-dianping/src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.swing.plaf.ListUI;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +24,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+//    @Resource
+//    private StringRedisTemplate stringRedisTemplate;
+//    @Override
+//    public Result oppo() {
+//        String key="shop:type:list";
+//        List<String> shopType = stringRedisTemplate.opsForList().range(key, 0, -1);
+//
+//
+//    }
 }
--- hm-dianping/hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,35 @@
 package com.hmdp;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.Shop;
+import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.CacheClient;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
+    @Resource
+    private CacheClient cacheClient;
+
+    @Resource
+    private ShopServiceImpl shopService;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+
+    @Test
+    void testSaveShop() throws InterruptedException {
+        Shop shop = shopService.getById(1L);
+        cacheClient.setWithLogicalExpire(CACHE_SHOP_KEY + 1L, shop, 10L, TimeUnit.SECONDS);
+    }
 
 }
